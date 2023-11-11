7e0065edc315848173e7b60972988c8b0c7ee4e1
Mia Borja <apk.borja@gmail.com>
2023-11-10 23:24:05 -0800

    updating default index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :categories
-  get 'products/index'
+  # resources :categories, only: [:index, :show] do
+  #   resources :products, only: [:index, :show]
+  # end
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'home#index'
 end
