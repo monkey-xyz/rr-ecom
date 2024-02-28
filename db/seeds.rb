e26a16fb7fb7215197a48f551bb1531017d079df
Mia Borja <apk.borja@gmail.com>
2024-02-27 23:54:10 -0800

    editing views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,30 @@ prods = Product.create([
   {
     name: "Product I",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    price: 4.3,
-    quantity: 4,
+    price: 9,
+    quantity: 10,
+    rating: 4,
   },
   {
     name: "Product II",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    price: 4.3,
-    quantity: 4,
+    price: 6.50,
+    quantity: 1,
+    rating: 5,
   },
   {
     name: "Product III",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    price: 4.3,
+    price: 9.20,
     quantity: 4,
+    rating: 3.5,
   },
   {
     name: "Product IV",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    price: 4.3,
-    quantity: 4,
+    price: 4.65,
+    quantity: 7,
+    rating: 4.75,
   }
 ])
 
@@ -37,5 +41,48 @@ cats = Category.create([
   },
   {
     name: "category_iii"
+  }
+])
+
+imags = Images.create([
+  {
+    img_url:"https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
+    img_alt: "Placeholder image",
+    product_id: 1,
+  },
+  {
+    img_url:"https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
+    img_alt: "Placeholder image",
+    product_id: 1,
+  },
+  {
+    img_url:"https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
+    img_alt: "Placeholder image",
+    product_id: 1,
+  },
+  {
+    img_url:"https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
+    img_alt: "Placeholder image",
+    product_id: 2,
+  },
+  {
+    img_url:"https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
+    img_alt: "Placeholder image",
+    product_id: 2,
+  },
+  {
+    img_url:"https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
+    img_alt: "Placeholder image",
+    product_id: 3,
+  },
+  {
+    img_url:"https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
+    img_alt: "Placeholder image",
+    product_id: 3,
+  },
+  {
+    img_url:"https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
+    img_alt: "Placeholder image",
+    product_id: 4,
   }
 ])
